d9fa6033bfefe5f82796ff7c2107e60b52d7da25
vanhngyen <53243467+vanhngyen@users.noreply.github.com>
2024-09-04 15:17:37 +0700

    first commit

--- HW2/src/main/java/EndPointGlobal.java
+++ HW2/src/main/java/EndPointGlobal.java
@@ -1,3 +1,3 @@
 public class EndPointGlobal {
-    public static final String EP_TOKEN_CORECTT = "api/paymentgateway/merchant/token";
+    public static final String EP_TOKEN_CORECTT = "https://smvab-test.epayjsc.com/vab/v2.0";
 }
